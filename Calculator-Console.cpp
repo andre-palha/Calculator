b8baec9f922df0a06cefd8680f2734d3ec7a93d1
andre-palha <jobs.andre@pm.me>
2023-04-03 12:30:38 +0100

    Added inputs from user

--- Calculator-Console.cpp
+++ Calculator-Console.cpp
@@ -1,20 +1,20 @@
-// Calculator-Console.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::string name;
+    std::cout << "What is your name? ";
+    std::getline(std::cin, name);
+    std::cout << "Hello " << name << "!\n";
+
+    int number1;
+    int number2;
+
+    std::cout << "Please enter your first number: ";
+    std::cin >> number1;
+    std::cout << "Please enter your second number: ";
+    std::cin >> number2;
+
+    std::cout << "The sum of your two numbers is: " << (number1 + number2);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
