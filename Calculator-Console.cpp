e9178f93a686abfa7a0de171da3b0819e1369d58
andre-palha <jobs.andre@pm.me>
2023-04-03 12:44:32 +0100

    Added switch for operator

--- Calculator-Console.cpp
+++ Calculator-Console.cpp
@@ -8,13 +8,36 @@ int main()
     std::getline(std::cin, name);
     std::cout << "Hello " << name << "!\n";
 
-    int number1;
-    int number2;
+    int number1, number2;
+    char op;
+    double result;
 
     std::cout << "Please enter your first number: ";
     std::cin >> number1;
     std::cout << "Please enter your second number: ";
     std::cin >> number2;
+    std::cout << "What is the operation - Type one (+, -, *, /): ";
+    std::cin >> op;
 
-    std::cout << "The sum of your two numbers is: " << (number1 + number2);
+    switch (op) {
+    case '+':
+        result = number1 + number2;
+        break;
+    case '-':
+        result = number1 - number2;
+        break;
+    case '*':
+        result = number1 * number2;
+        break;
+    case '/':
+        result = number1 / number2;
+        break;
+    default:
+        std::cout << "Wrong operator";
+        return 1;
+        break;
+    }
+
+    std::cout << "Result for " << op << ": " << result << std::endl;
+    return 0;
 }
